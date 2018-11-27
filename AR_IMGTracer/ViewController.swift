4964e6fba36f1d7901d29c96fe7aeebdee0e356d
emin.roblack@gmail.com <emin.roblack@gmail.com>
2018-11-27 10:49:12 +0100

    Add plane to scene. Working MVP

--- AR_IMGTracer/ViewController.swift
+++ AR_IMGTracer/ViewController.swift
@@ -7,13 +7,59 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+  @IBOutlet weak var sceneView: ARSCNView!
+  let configuration = ARImageTrackingConfiguration()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    sceneView.delegate = self
+    
+    let scene = SCNScene(named: "ARassets.scnassets/GameScene.scn")!
+    sceneView.scene = scene
+    
+    sceneView.autoenablesDefaultLighting = true
   }
+  
+  
+  
+  override func viewWillAppear(_ animated: Bool) {
+    
+    guard  let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "Photos", bundle: Bundle.main) else {
+      return print("No images avaialble")
+    }
+    
+    configuration.trackingImages = trackedImages
+    configuration.maximumNumberOfTrackedImages = 1
+    
+    sceneView.session.run(configuration)
+  }
+  
+  
+  
+  func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    
+    let node = SCNNode()
+
+    if let imageAnchor = anchor as? ARImageAnchor {
+      let planeScene = SCNScene(named: "ARassets.scnassets/plane.scn")
+      let planeNode = planeScene?.rootNode.childNodes.first!
+      
+      planeNode?.position = SCNVector3Zero
+      planeNode?.position.y = 0.15
+      
+      node.addChildNode(planeNode!)
+    }
+    
+    return node
+  }
+  
+  
 
 
 }
